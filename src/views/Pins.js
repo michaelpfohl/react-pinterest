71cb60a273f47c0721e65785388e570f1fb0b008
Michael Pfohl <rmpfohl@gmail.com>
2020-11-19 19:35:44 -0600

    create pin functional

--- src/views/Pins.js
+++ src/views/Pins.js
@@ -1,10 +1,68 @@
-import React from 'react';
-import UserPins from '../components/UserPins';
+import React, { Component } from 'react';
+import authData from '../helpers/data/authData';
+import pinsData from '../helpers/data/pinsData';
 
-export default function Pins() {
-  return (
-    <div>
-      <UserPins />
+import UserPins from '../components/UserPins';
+import AppModal from '../components/AppModal';
+import PinForm from '../components/Forms/PinForm';
+
+// export default function Pins() {
+//   state = {
+//     pins: [],
+//   };
+
+//   componentDid
+
+//   getUserPins = () => {
+//     const user = authData.getUid();
+//     pinsData.getAllPins().then((response) => {
+//       const userPins = Object.values(response).filter((pin) => pin.userId === user);
+//       this.setState({
+//         pins: userPins,
+//       });
+//     });
+//   }
+
+//   return (
+//     <div>
+//       <AppModal title={'Create Pin'} buttonLabel={'Create Pin'}>
+//         <PinForm onUpdate={this.getPins} />
+//       </AppModal>
+//       <UserPins pins={pins}/>
+//     </div>
+//   );
+// }
+
+class Pins extends Component {
+  state = {
+    pins: [],
+  }
+
+  componentDidMount() {
+    this.getUserPins();
+  }
+
+  getUserPins = () => {
+    const user = authData.getUid();
+    pinsData.getAllPins().then((response) => {
+      const userPins = Object.values(response).filter((pin) => pin.userId === user);
+      this.setState({
+        pins: userPins,
+      });
+    });
+  }
+
+  render() {
+    const { pins } = this.state;
+    return (
+      <div>
+        <AppModal title={'Create Pin'} buttonLabel={'Create Pin'}>
+          <PinForm onUpdate={this.getUserPins} />
+        </AppModal>
+        <UserPins pins={pins}/>
     </div>
-  );
+    );
+  }
 }
+
+export default Pins;
