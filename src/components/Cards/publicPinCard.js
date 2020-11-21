b94158b964c53cf9bbeba05cf2b7ab36f459c7e9
Michael Pfohl <rmpfohl@gmail.com>
2020-11-21 11:56:18 -0600

    change public cards to have no delete button

---
+++ src/components/Cards/publicPinCard.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  Button,
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
+import AppModal from '../AppModal';
+import BoardDropdown from '../BoardDropdown';
+
+class PublicPinCard extends Component {
+  render() {
+    const { pin } = this.props;
+    return (
+      <div>
+        <Card className='board-card'>
+          <CardImg
+            top
+            width='100%'
+            src={pin.imageUrl}
+            alt="Card image cap"
+          />
+          <CardBody>
+            <CardTitle tag="h5" className="card-title">{pin.name}</CardTitle>
+            <CardText className="card-description">
+                {pin.description}
+            </CardText>
+            <Button className="card-button">
+              <Link className="card-button-text" to={`/pins/${pin.firebaseKey}`}>View Pin</Link>
+            </Button>
+            <AppModal title={'Pin to Board'} buttonLabel={'Pin to Board'}>
+              <BoardDropdown pin={pin.firebaseKey}/>
+            </AppModal>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default PublicPinCard;
--- src/components/HomeComponent/index.js
+++ src/components/HomeComponent/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import pinsData from '../../helpers/data/pinsData';
-import PinCard from '../Cards/pinCard';
+import PublicPinCard from '../Cards/publicPinCard';
 
 class Home extends Component {
   state = {
@@ -21,7 +21,7 @@ class Home extends Component {
   render() {
     const { pins } = this.state;
     const showPins = () => (
-      pins.map((pin) => <PinCard key={pin.firebaseKey} pin={pin}/>)
+      pins.map((pin) => <PublicPinCard key={pin.firebaseKey} pin={pin}/>)
     );
     return (
       <div className="pin-container d-flex flex-wrap justify-content-center">
